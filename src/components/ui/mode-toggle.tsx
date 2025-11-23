f4121eed51397da4ef34fbc85e1487c09747a82e
Andrei Gălițianu <andrei@galitianu.com>
2025-11-23 18:08:18 +0100

    fix mode toggle on safari ios

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -46,7 +46,7 @@ export function ModeToggle() {
   }, [theme]);
 
   return (
-    <DropdownMenu>
+    <DropdownMenu modal>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' size='icon' className='rounded-full'>
           <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
